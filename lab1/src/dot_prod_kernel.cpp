eff2799dc56d3ff3237e381058e24965e00a189b
vishnu <devarakonda.vishnu5@gmail.com>
2021-10-05 09:23:17 -0700

    Remove pipeline

--- lab1/src/dot_prod_kernel.cpp
+++ lab1/src/dot_prod_kernel.cpp
@@ -20,8 +20,6 @@ void dot_prod_kernel(const float *a, const float *b, float *c, const int num_ele
   #pragma HLS array_partition variable=b factor=num_elems dim=0
 
   for(int i=0; i < num_elems; i++){
-    #pragma HLS dataflow
-    #pragma HLS stable variable=c
     #pragma HLS unroll
     *c = *c + (a[i]*b[i])
   }
